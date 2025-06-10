615a004a146c050b237d7379849c89af3fab103a
Hamza <151831165+Starist-tech@users.noreply.github.com>
2025-06-10 22:40:10 +0100

    Update main.js

--- Javascript_assignements/03_Numbres/main.js
+++ Javascript_assignements/03_Numbres/main.js
@@ -1,4 +1,4 @@
-h/*                     ------first------                                 */
+kh/*                     ------first------                                 */
 console.log("%c------first------", "color: #32CD32;");
 
 
